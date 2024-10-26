e578c7296fcf941402fdf418711cdc9775ca6706
Chivan <chivan@chivan.com>
2024-10-27 00:46:05 +0200

    poor mans merge

--- src/audiowrapper.h
+++ src/audiowrapper.h
@@ -117,7 +117,7 @@ Q_SIGNALS:
 
     void positionChanged(qint64 position);
 
-    void currentPlayingForRadiosChanged(const QString &title, const QString &nowPlaying);
+    void currentPlayingForRadiosChanged(const QString &title, const QString &artistOrStation);
 
     void seekableChanged(bool seekable);
 
